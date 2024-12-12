162bfaffdc893cd20b71eff479c39edce900415d
Divyansh Yadav <divyanshyadav20@gmail.com>
2024-12-12 19:26:07 +0530

    Initial proto for table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Table from "./components/table/Table";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="containers">
+      <div className="container one" />
+      <div className="container two" />
+      <div className="container three">
+        <Table />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/table/Table.tsx
@@ -0,0 +1,7 @@
+import "./Table.css";
+
+const Table = () => {
+  return <table id="projects-table"></table>;
+};
+
+export default Table;
